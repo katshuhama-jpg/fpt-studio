2c834becf5dc51bdc3575e8a5e1dabe1a59c0f0b
Tyler Dang <katshuhama@gmail.com>
2026-08-10 11:09:35 +0700

    auto update 2026-08-10 11:09:35

--- src/pages/organization/OrgStructureExplorer.tsx
+++ src/pages/organization/OrgStructureExplorer.tsx
@@ -240,7 +240,7 @@ export default function OrgStructureExplorer() {
         </div>
         <div className="flex-1 overflow-y-auto p-2 space-y-0.5">
           <TreeRow
-            unit={orgTree} depth={0} selectedId={selectedId}
+            unit={tree} depth={0} selectedId={selectedId}
             expanded={expanded} onToggle={toggleExpand} onSelect={selectUnit} query={treeQuery}
           />
         </div>
